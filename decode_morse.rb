59e78ba9f5b48466bceb38942f676638af3f89d8
Olivier-Kango <olivierkango@gmail.com>
2023-01-13 17:56:25 -0800

    Add the decode method

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,34 +1,54 @@
 class DecodeMorse
-   def decode_char(code)
+  def decode_char(code)
     morse_code = {
-      '.-'=> 'A',
-      '-...'=> 'B',
-      '-.-.'=> 'C',
-      '-..'=> 'D',
-      '.'=> 'E',
-      '..-.'=> 'F',
-      '--..'=> 'G',
-      '....'=> 'H',
-      '..'=> 'I',
-      '.---'=> 'J',
-      '-.-'=> 'K',
-      '.-..'=> 'L',
-      '--'=> 'M',
-      '-.'=> 'N',
-      '---'=> 'O',
-      '.--.'=> 'P',
-      '--.-'=> 'Q',
-      '.-.'=> 'R',
-      '...'=> 'S',
-      '-'=> 'T',
-      '..-'=> 'U',
-      '...-'=> 'V',
-      '.--'=> 'W',
-      '-..-'=> 'X',
-      '-.--'=> 'Y',
-      '--.'=> 'Z'
+      '.-' => 'A',
+      '-...' => 'B',
+      '-.-.' => 'C',
+      '-..' => 'D',
+      '.' => 'E',
+      '..-.' => 'F',
+      '--..' => 'G',
+      '....' => 'H',
+      '..' => 'I',
+      '.---' => 'J',
+      '-.-' => 'K',
+      '.-..' => 'L',
+      '--' => 'M',
+      '-.' => 'N',
+      '---' => 'O',
+      '.--.' => 'P',
+      '--.-' => 'Q',
+      '.-.' => 'R',
+      '...' => 'S',
+      '-' => 'T',
+      '..-' => 'U',
+      '...-' => 'V',
+      '.--' => 'W',
+      '-..-' => 'X',
+      '-.--' => 'Y',
+      '--.' => 'Z'
     }
 
     morse_code[code]
   end
+
+  def decode_word(word)
+    output = ''
+    arr_word = word.split
+    arr_word.each do |code|
+      output += decode_char(code)
+    end
+    output
+  end
+
+  def decode(phrase)
+    output = ''
+    arr_phrase = phrase.split('   ')
+    arr_phrase.each_with_index do |word, index|
+      output += index == arr_phrase.size - 1 ? decode_word(word) : "#{decode_word(word)} "
+    end
+    output
+  end
 end
+
+p DecodeMorse.new.decode('.-. ..- -... .. . ...')
