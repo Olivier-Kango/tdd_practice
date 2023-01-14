4b518d739b6b05272902a8fde49d0a2a68a41e16
Olivier-Kango <olivierkango@gmail.com>
2023-01-13 17:56:37 -0800

    Add the decode_word

--- spec/decode_morse_spec.rb
+++ spec/decode_morse_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe DecodeMorse do
       expect(result.decode_char('-.--')).to eq 'Y'
     end
   end
+
+  context 'Decode Morse Word' do
+    it 'Should decode a word' do
+      result = DecodeMorse.new
+      expect(result.decode_word('-- -.--')).to eq 'MY'
+      expect(result.decode_word('-. .- -- .')).to eq 'NAME'
+    end
+  end
+
+  context 'Decode Morse Word' do
+    it 'Should decode a word' do
+      result = DecodeMorse.new
+      expect(result.decode('--')).to eq 'M'
+      expect(result.decode('-.--')).to eq 'Y'
+      expect(result.decode('-- -.--')).to eq 'MY'
+      expect(result.decode('-. .- -- .')).to eq 'NAME'
+      expect(result.decode('-- -.--   -. .- -- .')).to eq 'MY NAME'
+    end
+  end
 end
