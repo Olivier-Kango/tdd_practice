5e8beb8a195bb73bcd852102ffda840225478275
Olivier-Kango <olivierkango@gmail.com>
2023-01-13 16:56:56 -0800

    Decode Morse Code Character

--- spec/decode_morse_spec.rb
+++ spec/decode_morse_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../decode_morse'
 
 RSpec.describe DecodeMorse do
-  context "Decode Morse Code Character" do
-    it "Should decode a character" do
+  context 'Decode Morse Code Character' do
+    it 'Should decode a character' do
       result = DecodeMorse.new
-      expect(result.decode_char(".-")).to eq "A"
+      expect(result.decode_char('.-')).to eq 'A'
+      expect(result.decode_char('--')).to eq 'M'
+      expect(result.decode_char('-.--')).to eq 'Y'
     end
   end
 end
